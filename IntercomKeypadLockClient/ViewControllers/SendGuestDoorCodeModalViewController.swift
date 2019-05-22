0cadd46b8bd120bbba716843fe08f819b5fb02a1
Ding <ding.ling@buzznog.com>
2019-05-23 04:55:07 +0800

    modal design

---
+++ IntercomKeypadLockClient/ViewControllers/SendGuestDoorCodeModalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SendGuestDoorCodeModalViewController.swift
+//  IntercomKeypadLockClient
+//
+//  Created by my on 5/23/19.
+//  Copyright © 2019 newlinks. All rights reserved.
+//
+
+import UIKit
+
+class SendGuestDoorCodeModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
